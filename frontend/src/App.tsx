7123055a6dafebcb3fa8a4c3d92c71ab6137bbc4
patarr19NEO <yurapatarr19@yandex.ru>
2025-12-02 21:05:22 +0300

    created header com and connected it in app and styles it in app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
-
+import Header from './comp/Header'
 import './App.css'
 
 export default function App() {
   return (
     <div className='App'>
+      <div className="header">
+        <Header />
+      </div>
        <div className="play-field">
         playfield
        </div>
