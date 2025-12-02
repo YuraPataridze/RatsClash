5ef85ab46d98d84a5c8649ebcba4e642bfe9df93
patarr19NEO <yurapatarr19@yandex.ru>
2025-12-02 22:43:45 +0300

    added assets; added 1st ver of coins showing

--- frontend/src/comp/PlayField/PlayField.tsx
+++ frontend/src/comp/PlayField/PlayField.tsx
@@ -1,9 +1,15 @@
+import { useState } from "react"
 import "./PlayFIeld.css"
 
 export default function PlayField() {
+    const [coins, setCoins] = useState<number>(982)
+
     return (
         <>
-            PlayField
+            <div className="coins">
+                <img src="./assets/coinICON.png" alt="coins" />
+                <h1>{coins}</h1>
+            </div>
         </>
     )
 }
