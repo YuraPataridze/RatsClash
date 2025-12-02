54bfabade00a99610dc91091beb97beed53c5397
patarr19NEO <yurapatarr19@yandex.ru>
2025-12-02 22:13:55 +0300

    did user name as usestate hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import Header from './comp/Header'
 import './App.css'
+import {useState } from 'react'
 
 export default function App() {
+  const [user_name, set_user_name] = useState<string>("pataridze19")
+
   return (
     <div className='App'>
       <div className="header">
-        <Header userName='pataridze19'/>
+        <Header userName={user_name}/>
       </div>
        <div className="play-field">
         playfield
