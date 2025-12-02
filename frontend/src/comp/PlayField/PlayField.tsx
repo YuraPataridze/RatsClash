0d7608289fa8991843f7ddbe20332eced8238f5d
patarr19NEO <yurapatarr19@yandex.ru>
2025-12-02 22:56:19 +0300

    added bar

--- frontend/src/comp/PlayField/PlayField.tsx
+++ frontend/src/comp/PlayField/PlayField.tsx
@@ -3,11 +3,39 @@ import "./PlayFIeld.css"
 
 export default function PlayField() {
     const [coins, setCoins] = useState<number>(982)
+    const [earnPerClick, setEarnPerCkick] = useState<number>(1)
+    const [coinsToLevUpm, setCoinsToLevUp] = useState<number>(10)
+    const [coinsPerSec, setCoinsPerSec] = useState<number>(0)
 
     return (
         <>
+            <div className="bar">
+                <div className="bar-content">
+                    <div className="bar-tab">
+                        <p>Earn per click</p>
+                        <div className="coin">
+                            <img src="./assets/coinICON.png" alt="coin" />
+                            <p>{earnPerClick}</p>
+                        </div>
+                    </div>
+                    <div className="bar-tab">
+                        <p>Coins to level up</p>
+                        <div className="coin">
+                            <img src="./assets/coinICON.png" alt="coin" />
+                            <p>{coinsToLevUpm}</p>
+                        </div>
+                    </div>
+                    <div className="bar-tab">
+                        <p>Auto earn p/sec</p>
+                        <div className="coin">
+                            <img src="./assets/coinICON.png" alt="coin" />
+                            <p>{coinsPerSec}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className="coins">
-                <img src="./assets/coinICON.png" alt="coins" />
+                <img src="" alt="coins" />
                 <h1>{coins}</h1>
             </div>
         </>
