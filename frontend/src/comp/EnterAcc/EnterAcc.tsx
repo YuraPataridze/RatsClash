46f3e82530a6ada9de205ff3d26a150ce092b6de
patarr19NEO <yurapatarr19@yandex.ru>
2025-12-10 22:21:35 +0300

    added entering html form

--- frontend/src/comp/EnterAcc/EnterAcc.tsx
+++ frontend/src/comp/EnterAcc/EnterAcc.tsx
@@ -3,7 +3,17 @@ import "./EnterAcc.css"
 export default function EnterAcc() {
     return (
         <>
-            EnterAcc
+            <div className="window">
+                <div className="content">
+                    <h2>Enter Account</h2>
+                    <form>
+                        <div className="form">
+                            <input type="password" id="psw-input" placeholder="Account unique code" />
+                            <button>Enter!</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
