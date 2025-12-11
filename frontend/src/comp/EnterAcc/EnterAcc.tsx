dded17c6630b18d4191938e39c2c88bf1ece2157
patarr19NEO <yurapatarr19@yandex.ru>
2025-12-12 00:04:21 +0300

    replace common form to .form to avoid future troubles

--- frontend/src/comp/EnterAcc/EnterAcc.tsx
+++ frontend/src/comp/EnterAcc/EnterAcc.tsx
@@ -1,17 +1,19 @@
 import "./EnterAcc.css"
 
 export default function EnterAcc() {
+    function handleEnter() {}
+
     return (
         <>
             <div className="window">
                 <div className="content">
                     <h2>Enter Account</h2>
-                    <form>
-                        <div className="form">
+                    <div className="form">
+                        <div className="form-content">
                             <input type="password" id="psw-input" placeholder="Account unique code" />
                             <button>Enter!</button>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </>
