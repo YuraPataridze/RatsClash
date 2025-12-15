b48d342c9d02117114706645d5c1c87cc3781a1e
patarr19NEO <yurapatarr19@yandex.ru>
2025-12-15 21:49:03 +0300

    did some shit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,13 @@ import {useState, useEffect} from 'react'
 
 
 export default function App() {
-  const [user_name, _set_user_name] = useState<string>("pataridze19")
+  const [user_name, _set_user_name] = useState<string>("pataridze19") // change in future
     
   //in the furute will work changing this state(with localstorage)
-  const [entered, _setEntered] = useState<boolean>(false)
+  const [entered, _setEntered] = useState<boolean>(true)
 
-  //enter account
-  const [enterCode, setEnterCode] = useState<string>()
+  //!-ENTER ACCOUNT-!//
+  const [enterCode, setEnterCode] = useState<string>('')
   const [loading, setLoading] = useState<boolean>(false)
 
   async function handleEnter() { // все данные, полученные с бека, мы будем передавать в playground comp через props или через localstorage или cokie
@@ -25,81 +25,88 @@ export default function App() {
       console.log(`%csending backdend user code: ${enterCode}`, "color: green")
       try {
         const response = await fetch('http://localhost:3000/api/enter', {
-            method: 'POST',                      headers: {
-                  'ContentType': "application/json",
+            method: 'POST',                      
+            headers: {
+                  'Content-Type': 'application/json',
               },
               body: JSON.stringify({user_code: enterCode}) // <- !!
-           })                  
+          })                  
           const gottent_from_back_data = await response.json()
           if (!response.ok) {
               console.error(`Something's wrong. Backend said: ${gottent_from_back_data.message} || ${gottent_from_back_data.status}`)
           }
             console.log(`%c${gottent_from_back_data.message}`, 'color: yellow')
-           console.log(`Coins of ${gottent_from_back_data.user_code} are ${gottent_from_back_data.coins}`)//as test showing coins in console
+          console.log(`Coins of ${gottent_from_back_data.user_code} are ${gottent_from_back_data.coins}`)//as test showing coins in console
         } catch (e) {
             setLoading(false)
             console.error(`%cUNKNOW ERROR: ${e}`, 'color: red')
         } finally {
-             setLoading(false)
-         }
+            setLoading(false)
+        }
     }
 
-    //playfield
+    //!-PLAYFIELD-!//
+    
+    // coins state
+    const [coins, setCoins] = useState<number>(
+      Number(localStorage.getItem("coins")) || 0
+    )
+    const [earnPerClick, setEarnPerClick] = useState<number>(1)
+    const [coinsToLevUp, setCoinsToLevUp] = useState<number>(100)
+    const [coinsPerSec, _setCoinsPerSec] = useState<number>(0)
+    
+     useEffect(() => {
+      localStorage.setItem("coins", coins.toString())
+    }, [coins])
+
+    // level  
+    const [_level, _setLevel] = useState<number>()
+    
+    // progress-bar states
+    const [progressBarVal, setProgressBarVal] = useState<number>(coins)
+    const [_maxProgressVal, _setMaxProgressVal] = useState<number>(coins)
+    
+    // manage click func
+    function manageClick() {
+        // test
+        console.log("CLICKED +1")
+    
+        //setCoins(Number(coins) + earnPerClick)
+        //setProgressBarVal(Number(coins) + 1)
+
+        setCoins(prev => prev + earnPerClick)
+      }
+
     useEffect(() => {
-            localStorage.setItem("coins", coins)
-        })
+      setProgressBarVal(coins)
+    }, [coins])
+
+    useEffect(() => {
+      if (coins >= coinsToLevUp) {
+        setCoinsToLevUp(prev => prev + 100)
+        setEarnPerClick(prev => prev + 1)
+        console.log('leveled up')
+      }
+    }, [coins])
+      // fetch data
+    const [loadingPF, setLoadingPF] = useState<boolean>(false)
     
-        // coins state
-        const [coins, setCoins] = useState<any>(localStorage.getItem("coins"))
-        const [earnPerClick, setEarnPerClick] = useState<number>(1)
-        const [coinsToLevUp, setCoinsToLevUp] = useState<number>(100)
-        const [coinsPerSec, _setCoinsPerSec] = useState<number>(0)
-    
-        // level
-        const [_level, _setLevel] = useState<number>()
-    
-        // progress-bar states
-        const [progressBarVal, setProgressBarVal] = useState<number>(coins)
-        const [_maxProgressVal, _setMaxProgressVal] = useState<number>(coins)
-    
-        // manage click fun
-        function manageClick() {
-            // test
-            console.log("CLICKED +1")
-    
-            setCoins(Number(coins) + earnPerClick)
-            setProgressBarVal(Number(coins) + 1)
-    
-            if (Number(coins) >= coinsToLevUp) {
-                console.log("leveled up!")
-                setCoinsToLevUp(coinsToLevUp + 100)
-                setEarnPerClick(earnPerClick + 2)
-            }
-    
-            if (coinsToLevUp >= 500) {
-                setCoinsToLevUp(coinsToLevUp + 500)
-            }
-        }
-    
-        // fetch data
-        const [loadingPF, setLoadingPF] = useState<boolean>(false)
-    
-        useEffect(() => {
-            async function fetchCoins() {
-                setLoadingPF(true)
-                try {
-                    const response = await fetch('http://localhost:3000/api/coins')
-                    if (!response.ok) throw new Error('Backend HTTP error')
-                    const data = await response.json()
-                    console.log(data)
-                    alert('Successfuly fetched data from backend')
-                } catch (err) {
-                    alert('You caught unknow error. More details see in browser console')
-                    console.error(err)
-                    setLoadingPF(false)
-                } finally {
-                    setLoadingPF(false)
-                }
+    useEffect(() => {
+        async function fetchCoins() {
+            setLoadingPF(true)
+            try {
+              const response = await fetch('http://localhost:3000/api/coins')
+              if (!response.ok) throw new Error('Backend HTTP error')
+              const data = await response.json()
+              console.log(data)
+              alert('Successfuly fetched data from backend')
+          } catch (err) {
+              alert('You caught unknow error. More details see in browser console')
+              console.error(err)
+              setLoadingPF(false)
+              } finally {
+                setLoadingPF(false)
+              }
             }
     
             fetchCoins()
